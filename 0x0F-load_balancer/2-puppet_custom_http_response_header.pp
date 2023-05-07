33fcd9ae8db6e3f3b1ee924d7266a4b3c57cb28f
Montanah <mititi.isaac.com>
2023-05-07 12:21:20 -0700

    puppet custom http response header

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,19 @@
+#Automating HTTP response header  creation using puppet
+
+exec {'update':
+  command => '/usr/bin/apt-get update',
+}
+
+package { 'nginx':
+    ensure => installed,
+  }
+
+file_line { 'add_custom_header':
+    path => '/etc/nginx/sites-available/default',
+    line => "add_header X-Served-By $hostname;",
+    match => 'server_name _;',
+  }
+
+exec {'run':
+  command => '/usr/sbin/service nginx restart',
+}
