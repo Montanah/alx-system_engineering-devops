4a663854e77d9c799ecabb76d74265f78315a113
Montanah <mititi.isaac.com>
2023-05-22 16:05:55 -0700

    script to export data in the CSV format

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-''' a script that returns information about
-an employee's TODO list progress & exports data in the CSV format '''
+''' a script that exports data in the CSV format '''
 
 if __name__ == "__main__":
 
