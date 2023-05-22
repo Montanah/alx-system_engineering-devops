e7d0c74a52c8ea59d655864f291f551b20ef781f
Montanah <mititi.isaac.com>
2023-05-22 15:07:21 -0700

    returns information about employee TODO list progress

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''A script that returns information about an employee's TODO list progress'''
+
+if __name__ == "__main__":
+	import requests
+	from sys import argv
+
+	employee_ID = int(argv[1])
+	r = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                     .format(employee_ID))
+	employee_name = r.json().get('name')
+
+	r = requests.get('https://jsonplaceholder.typicode.com/todos')
+    tasks = r.json()
+    all_tasks = [item for item in tasks if item.get('userId') == employee_ID]
+    done_tasks = [item for item in all_tasks if item.get('completed') is True]
+
+    print('Employee {} is done with tasks({}/{}):'.format(employee_name,
+                                                          len(done_tasks),
+                                                          len(all_tasks)))
+    for task in done_tasks:
+        print('\t {}'.format(task.get('title')))
