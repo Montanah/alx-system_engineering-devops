2a3d87c863caf46c0b9cca441fbf7e4a6b4afacd
Montanah <mititi.isaac.com>
2023-05-23 02:02:48 -0700

    script that saves & exports data in JSON format

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+''' script to save & export data in JSON format '''
+
+if __name__ == "__main__":
+    import json
+    import requests
+
+    r = requests.get('https://jsonplaceholder.typicode.com/users/')
+    users = r.json()
+
+    r = requests.get('https://jsonplaceholder.typicode.com/todos')
+    tasks = r.json()
+
+    all_tasks_dict = dict()
+
+    for employee in users:
+        emp_id = employee.get('id')
+        employee_name = employee.get('username')
+        emp_tasks = [item for item in tasks if item.get('userId') == emp_id]
+
+        emp_dict = {emp_id: []}
+        for task in emp_tasks:
+            emp_dict.get(emp_id).append({'task': task.get('title'),
+                                         'completed': task.get('completed'),
+                                         'username': employee_name})
+        all_tasks_dict.update(emp_dict)
+
+    with open('todo_all_employees.json', 'w+') as f:
+        json.dump(all_tasks_dict, f)
