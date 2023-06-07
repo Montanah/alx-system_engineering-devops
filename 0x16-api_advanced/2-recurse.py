a642a2793bf783a2260a96ed34ec075014efb2f2
Montanah <mititi.isaac@gmail.com>
2023-06-07 16:33:46 +0300

    a recursive function that queries the Reddit API and returns a list containing the titles of all hot articles for a given subreddit.

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -11,25 +11,20 @@ def recurse(subreddit, hot_list=[], after=''):
     '''
     returns full list of hot posts in a subreddit
     '''
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    url = 'https://www.reddit.com/r/{}/hot.json?after={}'.format(subreddit, after)
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code == 200:
-        data = response.json().get('data')
-        if data and data.get('children'):
-            children = data.get('children')
-            for post in children:
-                title = post.get('data').get('title')
-                hot_list.append(title)
+    try:
+        if r.json()['data']['dist'] == 0:
+            return None
+        for post in r.json()['data']['children']:
+            hot_list.append(post['data']['title'])
+    except (KeyError, IndexError):
+        if (after == ''):
+            return None
 
-            """Check if there are more pages"""
-            after = data.get('after')
-            if after:
-                return recurse(subreddit, hot_list, after)
-            else:
-                return hot_list
-        else:
-            return hot_list
-    else:
-        return None
+    if (r.json()['data']['after'] is None):
+        return hot_list
+
+    return recurse(subreddit, hot_list, r.json()['data']['after'])
