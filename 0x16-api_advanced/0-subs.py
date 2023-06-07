9297b249ec80012bbac804e3b01c68cdcebf0f40
Montanah <mititi.isaac@gmail.com>
2023-06-07 11:11:53 +0300

    A function that queries the Reddit API and returns the number of subscribers for a given subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-"""A function that queries the Reddit API and returns the number of subscribers for a given subreddit."""
+"""A function that queries the Reddit API and returns
+the number of subscribers for a given subreddit."""
+
 import requests
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers for a given subreddit."""
+    """Queries the Reddit API and returns the number
+    of subscribers for a given subreddit."""
 
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64)
+               AppleWebKit/537.36 (KHTML, like Gecko) '
                              'Chrome/51.0.2704.103 Safari/537.36'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
