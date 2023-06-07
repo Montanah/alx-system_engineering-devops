88b3e2e25bfa24b36d47d5ca598697d5c4f26bc6
Montanah <mititi.isaac@gmail.com>
2023-06-08 00:27:28 +0300

    Using strace

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -12,8 +12,7 @@ def top_ten(subreddit):
 
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers,
-                            allow_redirects=False)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
         for post in response.json().get('data').get('children')[:10]:
